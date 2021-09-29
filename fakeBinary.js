e5981556cc99fe128511e3303aafc002a11f2da2
Maynard <80284789+ManiacalMaynard@users.noreply.github.com>
2021-09-29 11:04:01 -0700

    Update fakeBinary.js

--- fakeBinary.js
+++ fakeBinary.js
@@ -11,6 +11,5 @@ const fakeBin = (x) => {
 
 //I use this line to test in an HTML document, safely ignore
 document.getElementById("primus").innerHTML = fakeBin('366058562030849490134388085')
-// returns "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
 
 
