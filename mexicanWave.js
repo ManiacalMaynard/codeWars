b3314234583a8f1c8c15c82acf86585882593ddd
ManiacalMaynard <maynard@maniacalindustries.com>
2021-12-16 15:34:30 -0800

    adds mexicanWave.js to solutions

--- mexicanWave.js
+++ mexicanWave.js
@@ -2,12 +2,20 @@ console.log('Que paso, gato?')
 
 // write a function that returns a series of strings, each of which will have a single capital letter in sequence. 
 //example: "Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"
-
+//might be fun to return to this and make it an animated thing? maybe?
+let waveOutput = []
 function wave(str){
-    return str
-  }
+    str = str.toLowerCase();
+    for (i=0; i < str.length; i++) {
+        let output = str.split('')
+        output[i] = output[i].toUpperCase()
+        waveOutput.push(output.join(''))
+    }
+    return waveOutput
+}
 
   
 // below used for testing, safely ignore
 const input = wave('CodeWars')
+document.getElementById('input').innerText = "wave('CodeWars')"
 document.getElementById('primus').innerText = input
