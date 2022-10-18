6ee6f58b52f701b5c3fd8885473f230e488af085
ManiacalMaynard <maynard@maniacalindustries.com>
2022-10-18 12:26:19 -0700

    minor fixes

--- stringToNumber.js
+++ stringToNumber.js
@@ -4,9 +4,13 @@ const stringToNumber = (str) => {
     return Number.parseInt(str);
 }
 
+
 //I use this line to test in an HTML document, safely ignore
+document.getElementById("input").innerHTML = 
+`stringToNumber('605')`
+
 document.getElementById("primus").innerHTML = 
-stringToNumber("-7")
+stringToNumber('605')
 
 /*    Test.assertEquals(stringToNumber("1234"),1234)
 Test.assertEquals(stringToNumber("605"), 605)
