10b53ff7a884cda6b3c7d29c5e8dcd78a72a9016
ManiacalMaynard <maynard@maniacalindustries.com>
2023-02-23 15:38:16 -0800

    adds a simple smallest number challenge answer

---
+++ smallestNumber.js
@@ -0,0 +1,27 @@
+console.log('The spice is the worm. The worm is the spice!')
+
+// let's set up the DOM variables!
+let input = document.getElementById('input')
+let result = document.getElementById('primus')
+let testy = [78,587,232,409,228]
+
+// set display text explaining the challenge, nice and easy today!
+const setChallengeText = (text) => {
+  document.getElementById('challenge').innerText = text
+}
+
+setChallengeText('Given an array of integers your solution should find the smallest integer.')
+
+// okay, spread opperator returns lowest number in the input array
+const compare = (arr) => {
+  let result = Math.min(...arr)
+  return result
+}
+
+// and set the values to their DOM elements
+const doit = () => {
+  input.innerText = testy
+  primus.innerText = compare(testy)
+}
+
+doit() // run it!
