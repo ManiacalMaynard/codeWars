0249e09b6744bacf5af817c11e1db51a46593e94
Maynard <maynard@maniacalindustries.com>
2025-05-09 13:11:45 -0400

    updates method to an on input update

--- reversedArray.js
+++ reversedArray.js
@@ -1,22 +1,21 @@
-console.log('Que paso, gato?')
-
 // describe the challenge from CodeWars
 document.getElementById('challenge').innerText = 'Given a random non-negative number, you have to return the digits of this number within an array in reverse order.'
 // samples: 35231 => [1,3,2,5,3] || 0 => [0]
 
-// the input
-let inputString = document.getElementById('input').innerText
+// the variables
+let input = document.getElementById('input')
+input.value = "" //clearing the input string on page load
 
-// the actual functionality
-const reverseArray = (n) => {
-    return n.split("").reverse()
+// update the output on input change
+input.oninput = function() { reverseArray() }
+
+let outputString = document.getElementById('primus')
+
+// the actual functionality for each challenge goes here
+const reverseArray = () => {
+    outputString.innerText = input.value.split("").reverse()
+    console.log(outputString.innerText)
 }
 
-// a button that calls the functionality
-const doIt = () => {
-    reverseArray()
-}
-
-// below used for testing, safely ignore
-const outputSting = reverseArray(inputString)
-document.getElementById('primus').innerText = outputSting
+// testing the entire script runs because I'm a little 'stitious
+console.log('Que paso, gato?')
